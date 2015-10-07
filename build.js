ea9a56b0ec0959797ef9a562cdff927fcc2303e7
Sam Ryan Furniture Committer <samryanfurniturewebsitecommitter@gmail.com>
2015-10-07 19:09:14 +0100

    Render the image navigation template completely in EJS

--- build.js
+++ build.js
@@ -4,6 +4,14 @@ var path = require("path"),
     child_process = require("child_process"),
     ejs = require("ejs");
 
+/* Read all of the images.json files in image_urls. */
+var image_json_files = fs.readdirSync("./image_urls"),
+    images_json_container = {};
+for (var i = 0, l = image_json_files.length; i < l; i++) {
+    var image_json_filename = image_json_files[i];
+    images_json_container[image_json_filename] = fs.readFileSync("./image_urls/" + image_json_filename + "/images.json");
+}
+
 files = fs.readdirSync("./pages");
 
 for (var i = 0, l = files.length; i < l; i++) {
@@ -17,7 +25,7 @@ for (var i = 0, l = files.length; i < l; i++) {
         fs.writeFileSync(inputFile + ".ejs", phpReturn.stdout);
 
         var ejsFile = fs.readFileSync(inputFile + ".ejs", "utf8"),
-            renderedEjs = ejs.render(ejsFile, {}, {filename: "ejs.ejs"});
+            renderedEjs = ejs.render(ejsFile, {"images_json_container": images_json_container}, {filename: "ejs.ejs"});
         fs.writeFileSync(filename, renderedEjs);
 
         process.stderr.write("Done.\n");
