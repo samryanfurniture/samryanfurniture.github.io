c2e45f91ea64a5bfbdc741a71ab721cd5f142a20
Sam Ryan Furniture Committer <samryanfurniturewebsitecommitter@gmail.com>
2015-10-07 17:50:30 +0100

    Add EJS step after PHP processing to build

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 var path = require("path"),
     fs = require("fs"),
-    child_process = require("child_process");
+    child_process = require("child_process"),
+    ejs = require("ejs");
 
 files = fs.readdirSync("./pages");
 
@@ -13,7 +14,11 @@ for (var i = 0, l = files.length; i < l; i++) {
         process.stderr.write("Creating " + filename + " from " + inputFile + "... ");
 
         var phpReturn = child_process.spawnSync("php", ["-f", inputFile])
-        fs.writeFileSync(filename, phpReturn.stdout);
+        fs.writeFileSync(inputFile + ".ejs", phpReturn.stdout);
+
+        var ejsFile = fs.readFileSync(inputFile + ".ejs", "utf8"),
+            renderedEjs = ejs.render(ejsFile);
+        fs.writeFileSync(filename, renderedEjs);
 
         process.stderr.write("Done.\n");
     }
