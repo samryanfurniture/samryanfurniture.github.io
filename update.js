b2683e38f7bc721c32133e71d9b53f4bf9847c2a
Sam Ryan Furniture Committer <samryanfurniturewebsitecommitter@gmail.com>
2016-01-28 14:22:45 +0000

    Associate albums with galleries in update.js

--- update.js
+++ update.js
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+var imgur_gallery = require("./imgur_gallery/imgur_gallery.js"),
+    gallery_pages = require("./gallery_pages.json");
+
+var accountName = "samryanfurniture";
+imgur_gallery.getUserAlbums(accountName, function (albumsInfo) {
+  /* Associate albums with gallery pages */
+  for (var i=0; i<albumsInfo.length; i++) {
+    for (var j=0;j<gallery_pages.length;j++) {
+      if (albumsInfo[i].title == gallery_pages[j].name) {
+        gallery_pages[j].albumInfo = albumsInfo[i];
+      }
+    }
+  }
+  for (var i=0; i<gallery_pages.length;i++) {
+    console.log(gallery_pages[i].albumInfo.id + ": " + gallery_pages[i].name + " ++ " + gallery_pages[i].filename);
+  }
+}, function (failureReason) {
+  log("ERROR from getUserAlbums: " + failureReasons[failureReason]);
+});
