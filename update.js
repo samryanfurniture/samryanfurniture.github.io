447419349fac3e81abac03d7716037d25aea5661
Sam Ryan Furniture Committer <samryanfurniturewebsitecommitter@gmail.com>
2016-01-28 14:50:04 +0000

    Write JSON files to image_urls from update.js

--- update.js
+++ update.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var imgur_gallery = require("./imgur_gallery/imgur_gallery.js"),
-    gallery_pages = require("./gallery_pages.json");
+    gallery_pages = require("./gallery_pages.json"),
+    fs = require("fs");
 
 var accountName = "samryanfurniture";
 imgur_gallery.getUserAlbums(accountName, function (albumsInfo) {
@@ -13,8 +14,21 @@ imgur_gallery.getUserAlbums(accountName, function (albumsInfo) {
     }
   }
   for (var i=0; i<gallery_pages.length;i++) {
-    console.log(gallery_pages[i].albumInfo.id + ": " + gallery_pages[i].name + " ++ " + gallery_pages[i].filename);
+    /*console.log(gallery_pages[i].albumInfo.id + ": " + gallery_pages[i].name + " ++ " + gallery_pages[i].filename);*/
+    (function (gallery_page) {
+      imgur_gallery.getUserAlbumImages(accountName, gallery_page.albumInfo.id, function (imagesInfo) {
+        try {
+          fs.writeFileSync("./image_urls/" + gallery_page.page + "/images.json", JSON.stringify(imagesInfo));
+        }
+        catch (e) {
+          fs.mkdirSync("./image_urls/" + gallery_page.page);
+          fs.writeFileSync("./image_urls/" + gallery_page.page + "/images.json", JSON.stringify(imagesInfo));
+        }
+      }, function (failureReason) {
+        log("ERROR from getUserAlbumImages: " + imgur_gallery.failureReasons[failureReason]);
+      });
+    })(gallery_pages[i]);
   }
 }, function (failureReason) {
-  log("ERROR from getUserAlbums: " + failureReasons[failureReason]);
+  log("ERROR from getUserAlbums: " + imgur_gallery.failureReasons[failureReason]);
 });
